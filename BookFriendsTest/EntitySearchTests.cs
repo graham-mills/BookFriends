6fea41da4e5515f148c1882fd97676ea3b274044
Graham Mills <graham.mills@outlook.com>
2020-10-12 17:47:36 +0100

    Adds tests for EntitySearch

---
+++ BookFriendsTest/EntitySearchTests.cs
@@ -0,0 +1,115 @@
+﻿using BookFriendsDataAccess;
+using BookFriendsDataAccess.Entities;
+using BookFriendsDataAccess.Repository;
+using BookFriendsDataAccess.Search;
+using Moq;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BookFriendsTest
+{
+    public class EntitySearchTests
+    {
+        private readonly Mock<IEntityRepository<CommunityGroup>> mockCommunityGroupRepo = new Mock<IEntityRepository<CommunityGroup>>();
+
+        private EntitySearch<CommunityGroup> uut;
+        private IList<CommunityGroup> repoEntities;
+
+        public EntitySearchTests()
+        {
+            repoEntities = new List<CommunityGroup>()
+            {
+                new CommunityGroup()
+                {
+                    Id = Guid.NewGuid(),
+                    Name = "group1",
+                    Keywords = "foo"
+                },
+                new CommunityGroup()
+                {
+                    Id = Guid.NewGuid(),
+                    Name = "group2",
+                    Keywords = "bar"
+                }
+            };
+        }
+
+        [SetUp]
+        public void Setup()
+        {
+            uut = new EntitySearch<CommunityGroup>(mockCommunityGroupRepo.Object);
+        }
+
+        // Non-matching search queries
+        [TestCase("foob"    , 0, 0)]
+        [TestCase(""        , 0, 0)]
+        [TestCase("group3"  , 0, 0)]
+        [TestCase(" "       , 0, 0)]
+        // Matching search queries
+        [TestCase("group1"  , 1, 1, 0)]
+        [TestCase("bar"     , 1, 1, 1)]
+        [TestCase("1"       , 1, 1, 0)]
+        [TestCase("group"   , 2, 2, 0, 1)]
+        [TestCase("foo  bar", 2, 2, 0, 1)]
+        [TestCase(" 1 2 "   , 2, 2, 0, 1)]
+        public void Search_QueryMatching(string searchQuery, int expectedResults, int expectedTotalResults, params int[] expectedIndexes)
+        {
+            int resultsToTake = repoEntities.Count;
+            int resultsToSkip = 0;
+            mockCommunityGroupRepo.SetupGet(repoEntities, repoEntities.Count);
+
+            EntitySearchResults<CommunityGroup> results = uut.Search(searchQuery, resultsToTake, resultsToSkip);
+
+            Assert.AreEqual(expectedResults, results.MatchedEntities.Count);
+            Assert.AreEqual(expectedTotalResults, results.TotalMatchedEntities);
+            for(int i = 0; i < expectedIndexes.Length; ++i)
+            {
+                Assert.IsTrue(results.MatchedEntities.Contains(repoEntities[expectedIndexes[i]]));
+            }
+        }
+
+        [TestCase(1, 0, 1, 2)]
+        [TestCase(1, 1, 1, 2)]
+        [TestCase(1, 2, 0, 2)]
+        [TestCase(3, 0, 2, 2)]
+        public void Search_TakeAndSkip(int resultsToTake, int resultsToSkip, int expectedResults, int expectedTotalResults)
+        {
+            string searchQuery = "foo bar";
+            mockCommunityGroupRepo.SetupGet(repoEntities, repoEntities.Count);
+
+            EntitySearchResults<CommunityGroup> results = uut.Search(searchQuery, resultsToTake, resultsToSkip);
+
+            Assert.AreEqual(expectedResults, results.MatchedEntities.Count);
+            Assert.AreEqual(expectedTotalResults, results.TotalMatchedEntities);
+        }
+
+        [TestCase("foo bar bar", 1, 0)]
+        [TestCase("bar foo foo", 0, 1)]
+        [TestCase("group1 group2 bar", 1, 0)]
+        [TestCase("group1 group2 foo", 0, 1)]
+        [TestCase("1 1 2", 0, 1)]
+        [TestCase("2 2 1", 1, 0)]
+        public void Search_ResultOrdering(string searchQuery, params int[] expectedIndexes)
+        {
+            int resultsToTake = repoEntities.Count;
+            int resultsToSkip = 0;
+            int expectedResults = repoEntities.Count;
+            int expectedTotalResults = repoEntities.Count;
+            mockCommunityGroupRepo.SetupGet(repoEntities, repoEntities.Count);
+
+            EntitySearchResults<CommunityGroup> results = uut.Search(searchQuery, resultsToTake, resultsToSkip);
+
+            Assert.AreEqual(expectedResults, results.MatchedEntities.Count);
+            Assert.AreEqual(expectedTotalResults, results.TotalMatchedEntities);
+            for (int i = 0; i < expectedIndexes.Length; ++i)
+            {
+                if(results.MatchedEntities.Count > i)
+                {
+                    Assert.AreEqual(repoEntities[expectedIndexes[i]], results.MatchedEntities[i]);
+                }
+            }
+        }
+    }
+}
--- BookFriendsTest/MockExtensions.cs
+++ BookFriendsTest/MockExtensions.cs
@@ -14,7 +14,7 @@ namespace BookFriendsTest
     {
         public static void SetupGet<TEntity>(this Mock<IEntityRepository<TEntity>> mock,
             IEnumerable<TEntity> itemsInRepo,
-            int expectedItemsToTake,
+            int? expectedItemsToTake,
             int itemsToReturn) where TEntity : class
         {
             // Return correct amount of entities if correct amount are requested (taken)
@@ -31,6 +31,19 @@ namespace BookFriendsTest
                 It.Is<int?>(t => t != expectedItemsToTake),
                 It.IsAny<int?>())
             ).Returns(itemsInRepo.Take(0));
+        }
+
+        public static void SetupGet<TEntity>(this Mock<IEntityRepository<TEntity>> mock,
+            IEnumerable<TEntity> itemsInRepo,
+            int itemsToReturn) where TEntity : class
+        {
+            // Return correct amount of entities if correct amount are requested (taken)
+            mock.Setup(x => x.Get(
+                It.IsAny<Expression<Func<TEntity, bool>>>(),
+                It.IsAny<Func<IQueryable<TEntity>, IOrderedQueryable<TEntity>>>(),
+                It.IsAny<int?>(),
+                It.IsAny<int?>())
+            ).Returns(itemsInRepo.Take(itemsToReturn));
         }
     }
 }
